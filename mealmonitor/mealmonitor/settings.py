ec15e948658f13f027ce525e4f368b525cd365f6
Beata Bąk <beata@dirtymonkey.co.uk>
2015-04-12 15:01:26 +0100

    Update timezone and language settings

--- mealmonitor/mealmonitor/settings.py
+++ mealmonitor/mealmonitor/settings.py
@@ -85,15 +85,15 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-GB'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/London'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
