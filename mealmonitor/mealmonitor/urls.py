a17df0f332bb6ca59d366521d8d57b9d61924b85
Beata Bąk <beata@dirtymonkey.co.uk>
2015-04-12 21:50:15 +0100

    Add url for the view which creates meals

--- mealmonitor/mealmonitor/urls.py
+++ mealmonitor/mealmonitor/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
 
     url(r'^$', 'meals.views.home', name='home'),
     url(r'^(?P<date>\d{4}-\d{2}-\d{2})$', 'meals.views.meal_list', name='meal_list'),
+    url(r'^meal_create/(?P<date>\d{4}-\d{2}-\d{2})$', 'meals.views.meal_create', name='meal_create'),
     url(r'^admin/', include(admin.site.urls)),
 ]
