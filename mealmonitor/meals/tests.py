fb1ed06d41052e2d42b128c1b532a0b9d2badc11
Beata Bąk <beata@dirtymonkey.co.uk>
2015-05-13 18:19:02 +0100

    Adding first unit test

--- mealmonitor/meals/tests.py
+++ mealmonitor/meals/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.core.urlresolvers import reverse
+
+from datetime import date, datetime
+
+
+
+class MealViewTests(TestCase):
+    def test_if_redirects_to_today(self):
+
+        """
+        Checks if '/' redirects to today's date
+        """
+        response = self.client.get(reverse('home'))
+        self.assertRedirects(response, date.today().isoformat())
